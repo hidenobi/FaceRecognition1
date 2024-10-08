61eaa15ebd48d239efeb460f4aad5ee45df6cad2
TuanVH <tuanvh@apero.vn>
2024-10-08 16:53:22 +0700

    init demo 1

---
+++ demo_1/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a pre-trained YOLOv10n model
+model = YOLO("yolov10n.pt")
+# Perform object detection on an image
+results = model("sources/test.png")
+
+# Display the results
+results[0].show()
